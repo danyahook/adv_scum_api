ecb3115fc3e970e91061e7d2d91405c63cb821eb
dkryuk <daniel.kryuk@websailors.pro>
2022-01-30 18:58:23 +0300

    Add nested serializer to site settings

--- src/api/serializers/site_serializer.py
+++ src/api/serializers/site_serializer.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 
 from api.models.site_model import Site
 
+from api.serializers.site_settings_serializer import SiteSettingsSerializer
+
 
 class SiteSerializer(serializers.ModelSerializer):
+    settings = SiteSettingsSerializer()
+
     class Meta:
         model = Site
         read_only_fields = ('site_uuid', 'is_valid', 'is_approved', 'created_at')
-        fields = '__all__'
+        fields = ('id', 'site_uuid', 'name', 'is_valid', 'is_approved', 'settings', 'created_at')
